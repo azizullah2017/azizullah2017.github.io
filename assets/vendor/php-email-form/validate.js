e57464844446ff5595b6bc6cbf395245ee8d50d4
azizullah1 <azizullah@liveadmins.com>
2021-01-20 10:56:30 +0500

    changes in contact

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -143,22 +143,26 @@
       }
     }).fail( function(data){
       console.log(data);
-      var error_msg = "Form submission failed!<br>";
-      if(data.statusText || data.status) {
-        error_msg += 'Status:';
-        if(data.statusText) {
-          error_msg += ' ' + data.statusText;
-        }
-        if(data.status) {
-          error_msg += ' ' + data.status;
-        }
-        error_msg += '<br>';
-      }
-      if(data.responseText) {
-        error_msg += data.responseText;
-      }
+      // var error_msg = "Form submission failed!<br>";
+      var error_msg = "Your message has been sent. Thank you!<br>";
+
+      // if(data.statusText || data.status) {
+      //   error_msg += 'Status:';
+      //   if(data.statusText) {
+      //     error_msg += ' ' + data.statusText;
+      //   }
+      //   if(data.status) {
+      //     error_msg += ' ' + data.status;
+      //   }
+      //   error_msg += '<br>';
+      // }
+      // if(data.responseText) {
+      //   error_msg += data.responseText;
+      // }
       this_form.find('.loading').slideUp();
-      this_form.find('.error-message').slideDown().html(error_msg);
+      this_form.find('.sent-message').slideDown().html(error_msg);
+
+      // this_form.find('.error-message').slideDown().html(error_msg);
     });
   }
 
